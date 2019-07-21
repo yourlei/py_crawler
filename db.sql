b0adf02e21e16c57f3c51bd61df6901f63d9de33
leiyu <yourlin127@gmail.com>
2019-07-21 20:02:44 +0800

    clean

--- db.sql
+++
@@ -1,9 +0,0 @@
--- 网页url
-CREATE TABLE links (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(512) DEFAULT '',
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT '0000-01-01 00:00:00',
-  PRIMARY KEY (`id`)
-)
--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 
-# 创建容器
-
-sudo docker run -dti \
-  --name scrap_py3 \
-  -v "$PWD":/var/www/code \
-  -w /var/www/code \
-  python:3.6-slim
